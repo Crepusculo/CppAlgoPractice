2710ecffd67d92109b748c3200253f330aa95eb5
Graham Tang <airfree1452@gmail.com>
2016-10-07 19:08:55 +0800

    QueueArraySample Push Analysis.

--- DataStructure/QueueArraySample.h
+++ DataStructure/QueueArraySample.h
@@ -8,23 +8,29 @@
 
 #include <cstddef>
 
-template <typename T>
+template<typename T>
 class QueueArraySample {
 private:
     constexpr static size_t SIZE = 100;
     size_t len;
     T *array = new T[SIZE];
+
     T *start = nullptr;
     T *end = nullptr;
 protected:
     bool is_full();
+
 public:
-    QueueArraySample():len(0),start(array),end(array){}
+    QueueArraySample() : len(0), start(array), end(array) {}
 
     bool push(T);
+
     bool fpush(T);
+
     T pop();
+
     size_t getSize();
+
     size_t getLen();
 };
 
@@ -42,10 +48,11 @@ bool QueueArraySample<T>::is_full() {
 
 template<typename T>
 bool QueueArraySample<T>::push(T target) {
-    if (getSize() <= getLen()){
-        std::clog<<"Queue is Full, Push Failed" << std::endl;
+    if (getSize() <= getLen()) {
+        std::clog << "Queue is Full, Push Failed" << std::endl;
         return false;
     }
+
     //  *
     //  *  start      end
     //  *    |         |
@@ -59,29 +66,30 @@ bool QueueArraySample<T>::push(T target) {
     //  *
 
     //  *
-    //  *    start           end
-    //  *      |              |
-    //  *    ------------------
-    //  *
-
-    //  *
-    //  *    start            end
-    //  *     |                |
-    //  *     ------------------
-    //  *
-
-    //  *
     //  *     end    start
     //  *      |       |
     //  *    ------------------
     //  *
+    if(this->end != &this->array[SIZE] && this->end != this->start){
+        *(this->end) =  target;
+        this->end++;
+        this->len++;
 
-    //  *
-    //  *    end
-    //  *    start
-    //  *     |
-    //  *     -----------------
-    //  *    ( len == 0 )
+        //  *
+        //  *    start            end
+        //  *      |               |
+        //  *    ------------------
+        //  *
+
+        //  *
+        //  *    start             end
+        //  *     |                 |
+        //  *     ------------------
+        //  *
+        if(this->end == &this->array[SIZE]) this->end = this->array;
+    }
+
+
 
     //  *
     //  *           end
@@ -89,7 +97,15 @@ bool QueueArraySample<T>::push(T target) {
     //  *            |
     //  *     -----------------
     //  *    ( len != 0 )
+    else if (SIZE <= this->len){
+        std::clog << "Failed to push, array has already fulled." << std::endl;
+    }
 
+    else{
+        std::clog<< "Some Status I can't found out." << std::endl;
+        return false;
+    }
+    return true;
 }
 
 template<typename T>
